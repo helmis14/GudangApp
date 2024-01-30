1ff07c925194d9cec54d85b861a286b81d3e5656
Helmi Sulaeman <76279980+helmis14@users.noreply.github.com>
2024-01-30 14:39:44 +0700

    tes

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@ require 'cek.php';
                         <div class="nav">
                         <a class="nav-link" href="permintaan.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Permintaan Barang
+                                Permintaan Barang  
                             </a>
                             <a class="nav-link" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
