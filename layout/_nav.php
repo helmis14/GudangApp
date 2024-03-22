8a9e0bbd8abfdedc83652358b4f3a3e1dc438eef
Helmi Sulaeman <sulaemanfc48@gmail.com>
2024-03-22 14:24:03 +0700

    add mobile responsive to 320px

--- layout/_nav.php
+++ layout/_nav.php
@@ -1,14 +1,14 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php">Plaza Oleos</a>
-    <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-    <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <li class="nav-item dropdown">
-        </li>
-        <li class="nav-item">
-            <span class="nav-link">
-                <div class="navbar-brand"></div>
-                Selamat datang, <?= $role; ?>
-            </span>
-        </li>
-    </ul>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 " id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    <div class="navbar-collapse collapse d-flex justify-content-between">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <span class="nav-link">
+                    <div class="navbar-brand"></div>
+                    Selamat datang, <?= $role; ?>
+                </span>
+            </li>
+        </ul>
+    </div>
 </nav>
